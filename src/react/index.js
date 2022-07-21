ba0e6930b2c8aac4e6570423a3a03b540b18e54a
erxiaowang417 <79679063+erxiaowang417@users.noreply.github.com>
2022-07-21 17:31:17 +0800

    Create index.js

---
+++ src/react/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Carplay from './App';
+import reportWebVitals from './reportWebVitals';
+ReactDOM.render( /*#__PURE__*/React.createElement(React.StrictMode, null, /*#__PURE__*/React.createElement(Carplay, null)), document.getElementById('root')); // If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+reportWebVitals();
